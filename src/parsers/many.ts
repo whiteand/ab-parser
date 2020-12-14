3e983345e9602ed669650e3fdaa817962cdaa5e8
whiteand <andrewbeletskiy@gmail.com>
2020-12-14 09:20:20 +0200

    Finished many and many1

--- src/parsers/many.ts
+++ src/parsers/many.ts
@@ -1,69 +1,17 @@
-import { PQueue } from "../ds/PQueue";
-import { collect, push, TList } from "../ds/list";
-import { Entry, P } from "../types";
-
-type TTask<T> =
-  | {
-      type: "yield";
-      values: T[];
-      rest: string;
-    }
-  | {
-      type: "iter";
-      parsedEntries: TList<Entry<T>>;
-      iterator: Iterator<Entry<T>>;
-    };
-
-function getPriority<T>(task: TTask<T>): number {
-  if (task.type === "yield") return task.rest.length;
-  if (!task.parsedEntries) return Infinity;
-  return task.parsedEntries.last.value.rest.length;
-}
+import { P } from "../types";
+import { many1 } from "./many1";
 
 export function many<T>(parser: P<T>): P<T[]> {
+  const many1Parser = many1(parser);
   return function* (text) {
-    const callStack = new PQueue<TTask<T>>(getPriority);
-    callStack.push({
-      type: "yield",
-      values: [],
-      rest: text,
-    });
-    callStack.push({
-      type: "iter",
-      parsedEntries: null,
-      iterator: parser(text),
-    });
-    while (callStack.length > 0) {
-      const task = callStack.pop() as TTask<T>;
-      if (task.type === "yield") {
-        yield { value: task.values, rest: task.rest };
-        continue;
-      }
-      let step = task.iterator.next();
-      const { parsedEntries } = task;
+    const many1Iterator = many1Parser(text);
+    while (true) {
+      const step = many1Iterator.next();
       if (step.done) {
-        if (!parsedEntries) continue;
-        const values = collect(parsedEntries, (e) => e.value);
-        const rest = parsedEntries.last.value.rest;
-        yield { value: values, rest };
-        continue;
+        break;
       }
-      while (!step.done) {
-        const entry = step.value;
-        callStack.push({
-          type: "iter",
-          parsedEntries: push(parsedEntries, entry),
-          iterator: parser(entry.rest),
-        });
-        step = task.iterator.next();
-      }
-      if (parsedEntries) {
-        callStack.push({
-          type: "yield",
-          values: collect(parsedEntries, (e) => e.value),
-          rest: parsedEntries?.last.value.rest,
-        });
-      }
+      yield step.value;
     }
+    yield { value: [], rest: text };
   };
 }
--- src/parsers/many1.ts
+++ src/parsers/many1.ts
@@ -1,27 +1,47 @@
-import { P } from "types";
+import { collect, push, TList } from "../ds/list";
+import { Entry, P } from "types";
+
+type TTask<T> =
+  | {
+      type: "yield";
+      value: T[];
+      rest: string;
+    }
+  | {
+      type: "get";
+      parsedEntries: TList<Entry<T>>;
+      iterator: Iterator<Entry<T>>;
+    };
 
 export function many1<T>(parser: P<T>): P<T[]> {
   return function* (text) {
-    const agendaCollected: T[][] = [];
-    const agendaRest: string[] = [];
-    const firstIt = parser(text);
-    while (true) {
-      const step = firstIt.next();
-      if (step.done) break;
-      agendaCollected.push([step.value.value]);
-      agendaRest.push(step.value.rest);
-    }
-    while (agendaRest.length > 0) {
-      const collected = agendaCollected.pop() as T[];
-      const rest = agendaRest.pop() as string;
-      yield { value: collected, rest };
-      const it = parser(rest);
-      while (true) {
-        const itEntry = it.next();
-        if (itEntry.done) break;
-        agendaCollected.push([...collected, itEntry.value.value]);
-        agendaRest.push(itEntry.value.rest);
+    const callStack: TTask<T>[] = [
+      {
+        type: "get",
+        parsedEntries: null,
+        iterator: parser(text),
+      },
+    ];
+    while (callStack.length > 0) {
+      const task = callStack.pop() as TTask<T>;
+      if (task.type === "yield") {
+        yield { value: task.value, rest: task.rest };
+        continue;
       }
+      const step = task.iterator.next();
+      if (step.done) continue;
+      const { value: entry } = step;
+      const newParsedEntries = push(task.parsedEntries, entry);
+      const newIterator = parser(entry.rest);
+      callStack.push(
+        task,
+        {
+          type: "yield",
+          value: collect(newParsedEntries, (e) => e.value),
+          rest: entry.rest,
+        },
+        { type: "get", parsedEntries: newParsedEntries, iterator: newIterator }
+      );
     }
   };
 }
